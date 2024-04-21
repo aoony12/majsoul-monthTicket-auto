73c4bca285e2d62fcb2da1ca16fa855730a3cffe
aoony12 <48904687+aoony12@users.noreply.github.com>
2024-04-21 16:13:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from ms.base import MSRPCChannel
 from ms.rpc import Lobby
 import ms.protocol_pb2 as pb
 
-uid = "your_uid"
-token = "your_token"
+uid = "39810219"
+token = "1c49cc83eaa445dbb33dc4f532c8bd63"
 
 deviceId = f"web|{uid}"
 
